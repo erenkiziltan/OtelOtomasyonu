5ff6fc42ad6e2e83056d0cd99f5f741d661d987b
erenkiziltan <111237317+erenkiziltan@users.noreply.github.com>
2022-12-12 18:42:01 +0300

    Add files via upload

---
+++ Admin/FrmGiris.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.XtraEditors;
+using OtelYeniProje.Entity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OtelYeniProje.Formlar.Admin
+{
+    public partial class FrmGiris : Form
+    {
+        public FrmGiris()
+        {
+            InitializeComponent();
+        }
+        DbOtelYeniEntities1 db = new DbOtelYeniEntities1();
+
+        private void BtnGiris_Click(object sender, EventArgs e)
+        {
+            var kullanici = db.TblAdmin.Where(x => x.Kullanici == TxtKullanici.Text && x.Sifre == TxtSifre.Text).FirstOrDefault();
+            if (kullanici != null)
+            {
+                Form1 frm = new Form1();
+                frm.kullanicirolu = kullanici.Rol;
+                frm.Show();
+                this.Hide();
+            }
+            else
+            {
+                XtraMessageBox.Show("Kullanıcı adı veya şifreniz yanlış, tekrar deneyin", "Hata", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+
+        private void FrmGiris_Load(object sender, EventArgs e)
+        {
+            Thread.Sleep(5000);
+        }
+
+        private void groupControl4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
